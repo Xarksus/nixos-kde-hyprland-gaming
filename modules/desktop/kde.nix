1db55eb3588febca3f475a61758da0d8e5e93daa
Xarksus <71927352+Xarksus@users.noreply.github.com>
2026-05-01 20:23:16 +0200

    Add KDE Plasma module

---
+++ modules/desktop/kde.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  services.desktopManager.plasma6.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.konsole
+    kdePackages.dolphin
+    kdePackages.spectacle
+    kdePackages.discover
+  ];
+}
