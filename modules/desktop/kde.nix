e0f3560b4661321929fffc2e897dc0a98d9cf22d
Xarksus <71927352+Xarksus@users.noreply.github.com>
2026-05-01 20:37:06 +0200

    Adjust KDE module for unstable and remove invalid plasma6 display manager setting

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -3,10 +3,6 @@
 {
   services.desktopManager.plasma6.enable = true;
 
-  services.displayManager.plasma6 = {
-    enable = true;
-  };
-
   environment.systemPackages = with pkgs; [
     kdePackages.konsole
     kdePackages.dolphin
