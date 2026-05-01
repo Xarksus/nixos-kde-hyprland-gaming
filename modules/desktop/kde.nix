0694b62f5c4e194440d5540c68c8738bf171ccd8
Xarksus <71927352+Xarksus@users.noreply.github.com>
2026-05-01 20:25:13 +0200

    Configure Plasma login manager (SDDM) for KDE/Hyprland setup

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -3,6 +3,12 @@
 {
   services.desktopManager.plasma6.enable = true;
 
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    theme = "breeze";
+  };
+
   environment.systemPackages = with pkgs; [
     kdePackages.konsole
     kdePackages.dolphin
