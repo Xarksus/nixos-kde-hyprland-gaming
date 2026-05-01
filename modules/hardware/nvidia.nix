42e4ecd8d45a65d442b50a77ba69fedb234eceeb
Xarksus <71927352+Xarksus@users.noreply.github.com>
2026-05-01 20:23:17 +0200

    Add NVIDIA hardware module

---
+++ modules/hardware/nvidia.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.graphics.enable = true;
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+  ];
+}
